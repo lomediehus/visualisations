7220e2ba07daab9d7b668c44b94f809a2e2a6b27
elinsteenKA <elin.steen@ka.se>
2020-12-21 10:28:06 +0100

    Add files via upload

---
+++ Begravning_gh/begravning_avtalen.js
@@ -0,0 +1,191 @@
+var avtalen = [
+  avtal1 = {
+    bransch: 1,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Kommun- och landstings&shy;anställda (undersköterskor, vårdbiträden med flera)</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med SKL, Sveriges Kommuner och Landsting</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst tio dagar per kalenderår.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs svårare sjukdom, bortgång, begravning, gravsättning av urna samt bouppteckning.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Exempelvis maka, make, registerad partner, sambo, barn, föräldrar, syskon, svärföräldrar, barnbarn samt mor- och farföräldrar.</p>",
+
+
+    yrke: ["aktiveringsassistent", "aktiveringspedagog", "aktivitetsledare", "arbetshandledare", "badmästare", "badpersonal", "behandlare", "behandlingsassistent", "boendeassistent", "boendehandledare", "boendestödjare", "brandman", "habiliteringsassistent", "ekonomibiträde", "habiliteringsbiträde", "habiliteringspedagog", "habiliteringspersonal", "obduktionstekniker", "omsorgsassistent", "socialpedagog", "stödassistent", "stödbiträde", "stödpedagog", "stödpersonal", "tvätteribiträde", "tvättmaskinskötare", "ambulanssjukvårdare", "anläggningsarbetare", "barnskötare", "barnsköterska", "biträden", "elevassistent", "fastighetsskötare", "fordonsförare", "fotvårdare", "fritidsledare", "förrådsarbetare", "kock/kokerska", "laboratoriebiträde", "måltidspersonal", "park- och trädgårdsarbetare", "personlig assistent", "skötare", "städare", "undersköterska", "vaktmästare", "vårdare", "vårdbiträde"]
+  },
+
+  // avtal2 = {
+  //   bransch: 2,
+  //   avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Räddningstjänsten</p>",
+  //   avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med SKL, Sveriges Kommuner och Landsting</h3>",
+  //   avtalstext1: "<p><strong>105,80 kr/tim</strong>från klockan 00.00 natten till julafton till klockan 24.00 annandag jul.</p><p><strong>127,00 kr/tim </strong> (natt-ob) mellan 00.00 och 06.00 på natten mot julaftonen, juldagen och annandagen.</p>",
+  //
+  //   yrke: ['brandman']
+  // },
+
+  avtal3 = {
+    bransch: 3,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i Svenska Kyrkan</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Svenska Kyrkan</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst tio dagar per kalenderår.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs svårare sjukdom, bortgång, begravning, gravsättning av urna samt bouppteckning.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Maka, make, registerad partner, sambo, barn, föräldrar, syskon, svärföräldrar, barnbarn samt mor- och farföräldrar.</p>",
+
+
+    yrke: ["assistent/församlingsarbete", "fritidsledare", "ungdomsledare", "barntimmeledare", "barnskötare", "diakoniassistent", "arbetsledare", "kyrkvaktmästare", "kyrkogårdsarbetare", "församlingshemsvärd", "husmor", "städare", "fastighetsskötare", "ekonomibiträde", "kontorsvaktmästare", "kyrkogårdsföreståndare"]
+  },
+
+  avtal4 = {
+    bransch: 4,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i privat vård</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sobona (fd KFS) Vård och omsorg</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst tio dagar per kalenderår.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs svårare sjukdom, bortgång, begravning, gravsättning av urna samt bouppteckning.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Maka, make, registerad partner, sambo, barn, föräldrar, syskon, svärföräldrar, barnbarn samt mor- och farföräldrar.</p>",
+
+    yrke: ["undersköterska", "vårdbiträde", "ekonomibiträde", "kock", "ungdomsassistent", "behandlingsassistent", "vaktmästare", "handledare"]
+  },
+
+  avtal5E = {
+    bransch: 4,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna, Bransch vård, behandlings- samt omsorgsverksamhet (E)</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> En eller flera dagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs bortgång, hastigt påkommande, allvarligare sjukdomsfall.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Medarbetares familj.</p>",
+
+    yrke: ["undersköterska", "vårdbiträde", "kökspersonal", "boendestödjare", "vårdare", "skötare", "lokalvårdare", "fastighetsskötare", "stödassistent"]
+  },
+
+  avtal5F = {
+    bransch: 4,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna, Bransch äldreomsorg (F)</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> En eller flera dagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs bortgång, hastigt påkommande, allvarligare sjukdomsfall.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Medarbetares familj.</p>",
+
+
+    yrke: ["undersköterska", "vårdbiträde", "kökspersonal", "lokalvårdare", "fastighetsskötare"]
+  },
+
+
+
+
+  avtal6 = {
+    bransch: 4,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med KFO, Kooperationens förhandlingsorganisation, Hälsa, vård och övrig omsorg</h3>",
+    avtalstext1:  "<p><span class='u-textStrong'>Antal lediga dagar:</span> En eller flera dagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Hastigt påkommande sjukdomsfall inom arbetstagarens familj eller nära anhörigs bortgång.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Make/maka, registerad partner, barn, barnbarn, syskon, föräldrar, svärföräldrar samt mor- och farföräldrar. </p>",
+
+
+    yrke: ["undersköterska", "vårdbiträde", "behandlingsassistent", "socialpedagog", "logopedassistent", "sjukgymnastassistent", "medlevare", "vård- och omsorgspersonal", "lokalvårdare", "köksbiträde", "vaktmästare"]
+  },
+
+  // avtal7 = {
+  //   bransch: 4,
+  //   avtalsoverskrift: "",
+  //   avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Arbetsgivaralliansen, Vård- och omsorg</h3>",
+  //   avtalstext1: "<p><strong>Max 102,44 kr/tim</strong> från klockan 07.00 på julafton till klockan 00.00 på vardagen närmast efter helgdagen, alltså torsdagen den 27 december 2018. Detta är den högsta möjliga ob-ersättningen. Ersättningen beräknas på månadslönen på heltid dividerat med 200. Här finns ett tak i beräkningen av månadslön som är 20&nbsp;488 kr/månad. Räkna ut vad ditt tillägg blir genom att skriva in din månadslön på heltid i rutan.</p><p>  <input class=\"pengar u-textMeta\" type=\"text\" name=\"pengar\" placeholder=\"Månadslön\" id=\"raknaLonKnapp\"><button type=\"button\" id=\"countButton\" class=\"u-textMeta u-textStrong Button countButton lessPadding\">Räkna ut</button></p><div id=\"dividedResultParent\"></div>",
+  //
+  //
+  //   yrke: ["undersköterska", "vårdbiträde", "behandlingsassistent", "socialpedagog", "vårdare", "medlevare", "fältarbetare", "sjukgymnastassistent", "vård- och omsorgspersonal", "lokalvårdare" , "kokerska", "kock", "vaktmästare", "ungdomsledare", "fritidsledare", "butikspersonal"]
+  // },
+
+  avtal8 = {
+    bransch: 5,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Personliga assistenter, ej kommunanställda</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> En eller flera dagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs bortgång, hastigt påkommande, allvarligare sjukdomsfall.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Medarbetares familj.</p>",
+
+
+    yrke: ['personlig assistent']
+  },
+
+  // avtal9 = {
+  //   bransch: 5,
+  //   avtalsoverskrift: "",
+  //   avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med KFO, Kooperationens Förhandlingsorganisation</h3>",
+  //   avtalstext1: "<p><strong>104,39 kr/tim</strong>tim från klockan 18.00 kvällen före julafton till klockan 07.00 på vardagen närmast efter helgdagen, alltså torsdagen den 27 december 2018.</p>",
+  //
+  //
+  //   yrke: ['personlig assistent']
+  // },
+
+  avtal10 = {
+    bransch: 5,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sobona (fd KFS)</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> Ej reglerat.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Ej reglerat.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Ej reglerat</p>",
+
+
+    yrke: ['personlig assistent']
+  },
+
+  avtal11 = {
+    bransch: 6,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Ambulanspersonal</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna</h3>",
+    avtalstext1:"<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst en dag, vid särskilda skäl ytterligare en dag samt vid nära anhörigs begravning högst två nödvändiga resdagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs bortgång, Nära anhörigs begravning, Plötsligt svårt sjukdomsfall hos hemmaboende nära anhörig.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> make, maka, barn, syskon, föräldrar, svärföräldrar, mor- och farföräldrar. Med make/maka jämställs person som medarbetaren sammanlever med under äktenskapsliknande förhållanden (det vill säga om man har samma mantalsskrivningsadress).</p>",
+
+    yrke: ['ambulanssjukvårdare']
+  },
+
+  // avtal12 = {
+  //   bransch: 7,
+  //   avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i privata storkök</p>",
+  //   avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Visita</h3>",
+  //   avtalstext1: "<p><strong>101,75 kr/tim</strong> från klockan 00.00 natten till julafton, juldagen och till annandagen klockan 24.00.</p>",
+  //
+  //
+  //   yrke: ["kock", "måltidspersonal", "köksbiträde", "ekonomibiträde"]
+  // },
+
+  avtal13 = {
+    bransch: 8,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Bussförare</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sveriges Bussföretag</h3>",
+    avtalstext1:  "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst en dag, vid begravning högst två resdagar, (totalt högst tio dagar per år).</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Begravning, också nära anhörigs bortgång, gravsättning, plötsligt svårt sjukdomsfall hos hemmaboende nära anhörig.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Exemepelvis make/maka, barn, syskon, föräldrar, svärföräldrar, mor- ochn farföräldrar.</p>",
+
+
+
+    yrke: ["bussförare", "spårvagnsförare", "verkstadspersonal", "mekaniker", "motorfordonsmekaniker", "reparatör", "lokalvårdare" , "garagepersonal", "buss, övriga", "chaufför"]
+  },
+
+  avtal14 = {
+    bransch: 8,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sobona (fd KFS)</h3>",
+    avtalstext1:  "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst en dag, vid begravning högst två resdagar, (totalt högst tio dagar per år).</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Begravning, också nära anhörigs bortgång, gravsättning, plötsligt svårt sjukdomsfall hos hemmaboende nära anhörig.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Exemepelvis make/maka, barn, syskon, föräldrar, svärföräldrar, mor- ochn farföräldrar.</p>",
+
+
+    yrke: ["bussförare", "spårvagnsförare", "verkstadspersonal", "mekaniker", "motorfordonsmekaniker", "reparatör", "lokalvårdare" , "garagepersonal", "buss, övriga", "chaufför"]
+  },
+
+  avtal15 = {
+    bransch: 9,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i jordbruk</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Gröna Arbetsgivare (fd SLA)</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> En dag, om nödvändigt högst två resdagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs bortgång, begravning, plötsligt svårt sjukdomsfall hos hemmaboende nära anhörig. </p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Make/maka, barn syskon, föräldrar, svärföräldrar, sambo, registerad partner eller person med motsvarande anhörigrelation.</p><p><span class='u-textStrong'>Villkor:</span> Anhållan ska göras i så god tid som möjligt, orsaken ska kunna styrkas på förhand, eller i efterhand om arbetsgivaren begär.</p>",
+
+
+    yrke: ["arbetsledare", "traktorförare", "maskinskötare", "maskinförare", "reparatör", "avbytare", "djurskötare", "lantarbetare"]
+  },
+
+  // avtal16 = {
+  //   bransch: 10,
+  //   avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i hästbranschen</p>",
+  //   avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalen med Almega Tjänsteföretagen</h3>",
+  //   avtalstext1:"<p>Ridlärare, hästskötare och övrig personal hos ridhusföretag. Hästskötare och övrig personal hos trav- och galopptränare. Banarbetare travbanor.<p></p>Ordinarie helgtillägg.</p>",
+  //
+  //
+  //   yrke: ["ridlärare", "hästskötare", "banarbetare"]
+  // },
+
+  avtal17 = {
+    bransch: 11,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Samhallanställda</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Samhall</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst en dag, i särskilda fall flera dagar. Vid begravning högst tre dagar med restid.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nödvändiga bestyr i samband med nära anhörigs bortgång, begravning, urnnedsättning, plötsligt svårt sjukdomsfall hos hemmaboende nära anhörig (högst en arbetsdag). </p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Make/maka, registerad partner, barn, barnbarn, syskon, föräldrar, svärföräldrar samt mor- och farföräldrar. Med make/maka jämställs person som arbetaren sammanlever med under äktenskapslika förhållanden.</p><p><span class='u-textStrong'>Villkor:</span> Ansök i god tid innan ledigheten ska börja, ange skälet och önskad tidperiod, nära anhörigs sjukdomsfall ska styrkas på begäran.</p>",
+
+    yrke: ["administratör", "chaufför", "driftsledare", "fastighetsskötare", "fönsterputsare", "instruktör", "internlärare", "kundtjänstmedarbetare", "lagerarbetare", "ledsagare", "lokalvårdare", "montör", "receptionist", "renhållningsarbetare", "omsorgsassistent", "serveringsbiträde", "servicevärd", "städare", "teamledare", "tvätterimedarbetare", "vaktmästare"]
+  },
+
+  avtal18 = {
+    bransch: 12,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Privatanställda städare</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Almega Tjänsteförbunden, serviceentreprenad</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Antal lediga dagar:</span> Högst en dag, vid begravning högst två nödvändiga resdagar.</p><p><span class='u-textStrong'>Då får du ledigt med betalning:</span> Nära anhörigs bortgång, begravning, plötsligt svårt sjukdomsfall hos hemmaboende nära anhörig.</p><p><span class='u-textStrong'>De räknas som anhöriga:</span> Ej specificerat i avtalet.</p>",
+
+
+    yrke: ["städare", "lokalvårdare"]
+  }
+
+]
