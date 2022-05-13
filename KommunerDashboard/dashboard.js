d727c3557a7d18d38c770d7e09f2569ac4972816
elinsteenKA <elin.steen@ka.se>
2022-05-13 11:18:03 +0200

    Add files via upload

--- KommunerDashboard/dashboard.js
+++ KommunerDashboard/dashboard.js
@@ -1,6 +1,6 @@
 const valjkommun = document.getElementById('valjkommun');
 // const data = '';
-const [aldreomsorgen, seniorer, forklaring] = [document.getElementById('aldreomsorgen'),  document.getElementById('seniorer'), document.getElementById('forklaring')];
+const [aldreomsorgen, seniorer, forklaring, forsorjningskvot] = [document.getElementById('aldreomsorgen'),  document.getElementById('seniorer'), document.getElementById('forklaring'), document.getElementById('forsorjningskvot')];
 
 
 const strans = [...document.getElementsByClassName('semitransparent')];
@@ -8,6 +8,7 @@ const strans = [...document.getElementsByClassName('semitransparent')];
 let kdata = [];
 
 let artal = "2002";
+let artal2 = "2002";
 
 let aldre_andel = '';
 let aldre_andel_gs = '';
@@ -21,6 +22,9 @@ let seniorer_skillnad_gs = '';
 
 let aldreomsorg_fyll = '';
 let seniorer_fyll = '';
+
+let forsorjningskvot_fyll;
+let fkvot_snitt;
 
 
 
@@ -63,7 +67,8 @@ function populateKommunDropdown() {
 function fillBoxes() {
 
   aldre_andel_gs = kdata[0]['Äldreomsorg 2020'];
-  aldre_skillnad_gs = kdata[0]['Äldreomsorg 2002-2020'];
+  aldre_skillnad_gs = kdata[0]['Äldreomsorg 02-20'];
+  fkvot_snitt = kdata[0]['Försörjningskvot 2020'];
 
 
   aldreomsorg_fyll = `
@@ -77,24 +82,44 @@ function fillBoxes() {
   aldreomsorgen.innerHTML = aldreomsorg_fyll;
 
   seniorer_andel_gs = kdata[0]["65+ år 2020"];
-  seniorer_skillnad_gs = kdata[0]["Andel 65+ 2002-2020"];
+  seniorer_skillnad_gs = kdata[0]["65+ år 2002-2020"];
 
   seniorer_fyll = `
     <h3>Andel av befolkningen 2020:<br><span class="storfet">XX%</span></h3>
-    <p>Genomsnitt för Sverige: ${seniorer_andel_gs}%</p>
+    <p>Genomsnitt för Sverige: ${$.number(seniorer_andel_gs, 1, ',', '&nbsp;')}%</p>
     <hr class='rounded -spacingBottomXS'>
-    <h3>Skillnad mot ${artal}:<br><span class="storfet"> XX</span></h3>
-    <p>Genomsnitt för Sverige: ${seniorer_skillnad_gs > 0 ? '+' + seniorer_skillnad_gs : seniorer_skillnad_gs}</p>
+    <h3>Skillnad mot ${artal2}:<br><span class="storfet"> XX</span></h3>
+    <p>Genomsnitt för Sverige: ${seniorer_skillnad_gs > 0 ? '+' + $.number(seniorer_skillnad_gs, 1, ',', 'nbsp;') : $number(seniorer_skillnad_gs, 1, ',', '&nbsp;')}</p>
   `
+
+  forsorjningskvot_fyll = `
+    <h3>2020:<br><span class="storfet">X,XX</span></h3>
+      <p>Genomsnitt för Sverige: ${$.number(fkvot_snitt, 2, ',', '&nbsp;')}%</p>
+      <hr class='rounded u-spacingBottomXS'>
+
+  `;
+
 
   seniorer.innerHTML = seniorer_fyll;
   forklaring.innerHTML = '';
+  forsorjningskvot.innerHTML = forsorjningskvot_fyll;
 }
 
 
 
 
 valjkommun.addEventListener("change", function(){
+
+  let undernollsex = [];
+  let nollsextillnollsju;
+  let nollsjutillnollatta;
+  let nollattatillnollnio;
+  let nollniotillett;
+  let etttillettett;
+  let etttilletttva;
+
+
+
   let num = 0;
   if (this.value === 'valjkommun') {
     fillBoxes();
@@ -103,24 +128,46 @@ valjkommun.addEventListener("change", function(){
 
   kdata.forEach((item, i) => {
 
+
+
     if (item.Kommun === this.value) {
 
+      // console.log(item['Försörjningskvot 2020'])
+
       aldre_andel = item['Äldreomsorg 2020'];
-      // aldre_andel_gs = kdata[0]['Äldreomsorg 2020'];
-      aldre_skillnad = item['Äldreomsorg 2002-2020'];
-      // aldre_skillnad_gs = kdata[0]['Äldreomsorg 2002-2020'];
+
+      if (item['Äldreomsorg 02-20'] != '') {
+        aldre_skillnad = item['Äldreomsorg 02-20'];
+        artal = "2002";
+      } else if (item['Äldreomsorg 03-20'] != '') {
+        aldre_skillnad = item['Äldreomsorg 03-20'];
+        artal = '2003';
+        console.log(artal)
+      } else if (item['Äldreomsorg 04-20'] != '') {
+        aldre_skillnad = item['Äldreomsorg 04-20'];
+        artal = "2004";
+      } else if (item['Äldreomsorg 09-20'] != '') {
+        aldre_skillnad = item['Äldreomsorg 09-20'];
+        artal = "2009";
+      }
+
+
+
       seniorer_andel = item['65+ år 2020'];
-      seniorer_skillnad = item['Andel 65+ 2002-2020'];
+      seniorer_skillnad = item['65+ år 2002-2020'];
+
+      console.log(aldre_skillnad === '')
+
 
       aldreomsorgen.innerHTML = '';
 
-      if (item.Kommun === "Knivsta") {
-        artal = "2006";
-
-      } else {
-        artal = "2002"
-
-      }
+      // if (item.Kommun === "Knivsta") {
+      //   artal = "2006";
+      //
+      // } else {
+      //   artal = "2002"
+      //
+      // }
 
       aldreomsorg_fyll = `
         <h3>Andel av budgeten 2020:<br><span class="storfet">${$.number(aldre_andel, 1, ',', '&nbsp;')}%</span></h3>
@@ -134,16 +181,11 @@ valjkommun.addEventListener("change", function(){
         <h3>Andel av befolkningen 2020:<br><span class="storfet">${$.number(seniorer_andel, 1, ',', '&nbsp;')}%</span></h3>
         <p>Genomsnitt för Sverige: ${$.number(seniorer_andel_gs, 1, ',', '&nbsp;')}%</p>
         <hr class='rounded u-spacingBottomXS'>
-        <h3>Skillnad mot ${artal}:<br><span class="storfet">${seniorer_skillnad > 0 ? '+' + $.number(seniorer_skillnad, 1, ',', '&nbsp;') : $.number(seniorer_skillnad, 1, ',', '&nbsp;')}</span></h3>
+        <h3>Skillnad mot 2002:<br><span class="storfet">${seniorer_skillnad > 0 ? '+' + $.number(seniorer_skillnad, 1, ',', '&nbsp;') : $.number(seniorer_skillnad, 1, ',', '&nbsp;')}</span></h3>
         <p>Genomsnitt för Sverige: ${seniorer_skillnad_gs > 0 ? '+' + $.number(seniorer_skillnad_gs, 1, ',', '&nbsp;') : $.number(seniorer_skillnad_gs, 1, ',', '&nbsp;')}</p>
       `
 
 
-      // function getKeyByValue(object, value) {
-      //     return Object.keys(object).find(key => object[key] === value);
-      //   }
-      //
-      // console.log(getKeyByValue(item, aldre_skillnad))
 
 
 
@@ -156,19 +198,39 @@ valjkommun.addEventListener("change", function(){
       let skillnad1 = (aldre_skillnad >= 0 ? 'ökat' : 'minskat');
       let andel2 = (seniorer_andel > seniorer_andel_gs ? 'större' : 'mindre');
       let skillnad2 = (seniorer_skillnad >= 0 ? 'ökat' : 'minskat');
+      let fkvot = item['Försörjningskvot 2020'];
+      fkvot_snitt = kdata[0]['Försörjningskvot 2020'];
 
       let forklaring_fyll = `
-      <p class='u-textMeta'>I ${kommunnamn} går en ${andel1} andel av budgeten till äldreomsorgen än genomsnittet för Sverige. Andelen har ${skillnad1} sedan ${artal}.<br>Andelen seniorer är ${andel2} än genomsnittet för Sverige. Andelen har ${skillnad2} sedan ${artal}.</p>
-      `
+      <p>I ${kommunnamn} går en ${andel1} andel av budgeten till äldreomsorgen än genomsnittet för Sverige. Andelen har ${skillnad1} sedan ${artal === '2002' ? artal : artal + '*'}.</p><p>Andelen seniorer är ${andel2} än genomsnittet för Sverige. Andelen har ${skillnad2} sedan ${artal2}.</p>
+      `;
 
-      if (kommunnamn === "Knivsta") {
-        forklaring_fyll += "<p class='u-textMeta'>(För Knivsta visas 2006 som startår, eftersom kommunen inte fanns 2002.)</p>"
+      if (artal != "2002") {
+        forklaring_fyll = `
+        <p>I ${kommunnamn} går en ${andel1} andel av budgeten till äldreomsorgen än genomsnittet för Sverige. Andelen har ${skillnad1}. Skillnaden för Sverige är sedan 2002. För ${kommunnamn} saknas den siffran, därför visas istället skillnaden sedan ${artal}.</p><p>Andelen seniorer är ${andel2} än genomsnittet för Sverige. Andelen har ${skillnad2} sedan ${artal2}.</p>
+        `;
+
+
       }
+
+      forsorjningskvot_fyll = `
+        <h3>2020:<br><span class="storfet">${$.number(fkvot, 2, ',', '&nbsp;')}%</span></h3>
+          <p>Genomsnitt för Sverige: ${$.number(fkvot_snitt, 2, ',', '&nbsp;')}%</p>
+          <hr class='rounded u-spacingBottomXS'>
+          <p>Försörjningskvoten kan förklaras som att en person i arbetsför ålder har  ${$.number(fkvot, 2, ',', '&nbsp;')} barn och gamla att försörja. (Summan av alla 0-19 och över 64 år, delat i antal personer 20-64 år.)</p>
+      `;
+
+
+      // if (kommunnamn === "Knivsta") {
+      //   forklaring_fyll += "<p class='u-textMeta'>(För Knivsta visas 2006 som startår, eftersom kommunen inte fanns 2002.)</p>"
+      // }
 
       forklaring.innerHTML = forklaring_fyll;
 
+      forsorjningskvot.innerHTML = forsorjningskvot_fyll;
 
-      visamerknapp.style.display = "inline";
+
+      // visamerknapp.style.display = "inline";
 
 
 
@@ -177,16 +239,11 @@ valjkommun.addEventListener("change", function(){
     }
 
 
-      if (item["Äldreomsorg 2002-2020"] < 0 && item["Andel 65+ 2002-2020"] > 0) {
-
+      if (item["Äldreomsorg 2002-2020"] < 0 && item["65+ år 2002-2020"] > 0) {
         // console.log(item.Kommun)
         // console.log(num)
-          num++;
+        //   num++;
       }
-
-
-
-
 
 
     })
@@ -208,22 +265,20 @@ valjkommun.addEventListener("change", function(){
 })
 
 let visamer = document.getElementById('visamer');
-let visamerknapp = document.getElementById('visamerknapp');
-
-function mer(){
-
-}
-
-visamerknapp.addEventListener('click', function(){
+// let visamerknapp = document.getElementById('visamerknapp');
 
 
-  if (visamer.style.display === "flex") {
-    visamer.style.display = "none";
-  } else {
-    visamer.style.display = "flex";
-  }
 
-})
+// visamerknapp.addEventListener('click', function(){
+//
+//
+//   if (visamer.style.display === "flex") {
+//     visamer.style.display = "none";
+//   } else {
+//     visamer.style.display = "flex";
+//   }
+//
+// })
 
 
 // window.onresize = function() {
