08109bea0a0a10eb4277beb60140139767b3c8c6
elinsteenKA <elin.steen@ka.se>
2022-09-20 11:41:49 +0200

    Add files via upload

---
+++ FormTest_codeHub/script.js
@@ -0,0 +1,28 @@
+
+console.log('skriptet körs')
+
+  function generateTxtFile(text){
+    var textFile = null;
+    // var data = new Blob([text], {type: 'text/plain'});
+    var data = new Blob([text], {type: 'application/json'});
+
+
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    return textFile;
+  }
+
+  (function () {
+    var btnCreateFile = document.getElementById('btnCreateFile'),
+    dummyText = document.getElementById('dummyText');
+
+    btnCreateFile.addEventListener('click', function () {
+      var link = document.getElementById('downloadFile');
+      link.href = generateTxtFile(dummyText.innerText);
+      link.style.display = 'inline-block';
+    }, false);
+  })();
