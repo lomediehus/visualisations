5c217bddf2023de4a106269668db52b5f34a1809
elinsteenKA <elin.steen@ka.se>
2022-09-27 10:26:04 +0200

    Add files via upload

--- sharetest/share.js
+++ sharetest/share.js
@@ -1,3 +1,9 @@
 console.log("script is running")
 
-console.log(document.querySelector('meta[property="og:title"]').content)
+let host = window.location.host;
+if (host.includes("github")) {
+  document.querySelector("link[rel='shortcut icon']").href = "favicon2.ico";
+  console.log('den finns på github')
+}
+
+console.log(document.querySelector('meta[property="og:title"]').content);
