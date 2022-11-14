ae72af84cd1ac35dcd09bfda6b4dce207cef4985
Karl Martinsson <36486318+karlmartinsson@users.noreply.github.com>
2022-11-14 12:52:37 +0100

    Add files via upload

--- inflationen_raknesnurra/inflation.js
+++ inflationen_raknesnurra/inflation.js
@@ -1,8 +1,19 @@
+
+
 function printNumber() {
     console.log("hej")
+   
+
     let number = document.getElementById("formvalue");
     let numberplace = document.getElementById("resultat");
-    numberplace.innerHTML = "Om din lön var <b>" + number.value + " kr</b> i oktober förra året är den värd <b>" + String(Math.round(parseInt(number.value) * 0.8)) + " </b>kr idag.";
+
+    let index_2021 = 345.74;
+    let index_2022 = 383.21;
+    let nylon = parseInt(number.value) * index_2021 / index_2022;
+    let skillnad = parseInt(number.value) - nylon;
+    let output_string = "Din lön på <b>" + number.value + " kr </b> har minskat med <b>" + Math.round(skillnad) + " kr</b> kronor på ett år och är i dag värd <b>" + Math.round(nylon) + " kr</b> i priserna som gällde i september 2021.";
+
+    numberplace.innerHTML = output_string;
 }
 
 document.getElementById("button").addEventListener("click", printNumber);
