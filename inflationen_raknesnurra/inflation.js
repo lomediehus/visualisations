fcfc4049d8bf8419168d2dfa812348f24f507a70
Karl Martinsson <36486318+karlmartinsson@users.noreply.github.com>
2023-01-12 09:50:53 +0100

    Add files via upload

--- inflationen_raknesnurra/inflation.js
+++ inflationen_raknesnurra/inflation.js
@@ -22,8 +22,19 @@ function printNumber() {
     let nylon = parseInt(number.value) * index_2021 / index_2022;
     let skillnad = parseInt(number.value) - nylon;
 
+
+    let fiktivlon = Math.round((parseInt(number.value) * ((index_2022 - index_2021) / index_2021)));
+
+    console.log(fiktivlon)
+
     console.log(number.value)
-    let output_string = "<p>Prisökningarna, eller inflationen, var i " + månad + " <b>" + formatPercent.to(inflationen) + "</b>.</p> <p>Din lön på <b>" + formatKronor.to(parseInt(number.value)) + "</b> har minskat i värde med <b>" + formatKronor.to(Math.round(skillnad)) + "</b> kronor på ett år och är i dag värd <b>" + formatKronor.to(Math.round(nylon)) + "</b> i priserna som gällde i " + månad + " " + år + ".</p><p><a href='" + url + "' target ='_blank'>Källa: SCB, (KPIF)</a></p>";
+    let output_string = "<p>Prisökningarna, eller inflationen, var i " + månad + " <b>" + formatPercent.to(inflationen) + "</b>.</p> <p>Din lön på <b>" + formatKronor.to(parseInt(number.value)) + "</b> har minskat i värde med <b>" + formatKronor.to(Math.round(skillnad)) + "</b> kronor på ett år och är i dag värd <b>" + formatKronor.to(Math.round(nylon)) + "</b> i priserna som gällde i " + månad + " " + år + ".</p>";
+
+    output_string += "<p>Din lön skulle behöva öka med <b>" + formatKronor.to(fiktivlon) + "</b> i dagens penningvärde för att vara lika mycket värd som i november 2021.</p>"
+
+    output_string += "<p><a href='" + url + "' target ='_blank'>Källa: SCB, (KPIF)</a></p>"
+
+
 
     numberplace.innerHTML = output_string;
 
