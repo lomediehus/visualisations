32770cbbc1c95fb276afa4a41f0a0bb57affddda
elinsteenKA <elin.steen@ka.se>
2023-05-30 08:41:43 +0200

    Add files via upload

--- pensionSlider2023m_staplar/pension1.js
+++ pensionSlider2023m_staplar/pension1.js
@@ -149,230 +149,232 @@ var data = [
 
 
 
-
-function modifyOffset() {
-  var el, newPoint, newPlace, offset, siblings, k;
-  width  = this.offsetWidth;
-  console.log(this)
-
-  newPoint = (this.value - this.getAttribute("min")) / (this.getAttribute("max") - this.getAttribute("min"));
-  // was originally -1, changed to -5 to push starting point to left
-  offset   = -5;
-  if (newPoint < 0) { newPlace = 0;  }
-  else if (newPoint > 1) { newPlace = width; }
-  else { newPlace = width * newPoint + offset; offset -= newPoint;}
-  siblings = this.parentNode.childNodes;
-  for (var i = 0; i < siblings.length; i++) {
-      sibling = siblings[i];
-      if (sibling.id == this.id) { k = true; }
-      if ((k == true) && (sibling.nodeName == "OUTPUT")) {
-          outputTag = sibling;
-      }
-  }
-  outputTag.style.left       = newPlace + "px";
-  outputTag.style.marginLeft = offset + "%";
-  outputTag.innerHTML        = slider.value + "%";
+//Function for the slider and bar chart, the funtion is called once when document loads, and after window is resized in the executeAfterResize function
+function doAllStuff(){
 
 
-  helpension.value = siffror[this.value].Premiepension + siffror[this.value].Inkomstpension + siffror[this.value].Garantipension + siffror[this.value].Tjänstepension;
-  deltidslon.value = (26800 * this.value) / 100;
-  procent.value = Math.round((helpension.value/deltidslon.value)*100) + "%";
-  output.innerHTML = slider.value + "%";
+    function modifyOffset() {
+    var el, newPoint, newPlace, offset, siblings, k;
+    width  = this.offsetWidth;
+    console.log(this)
 
-  // Update the data objecgt
-  data[0].Value = +deltidslon.value;
-  data[1].Value = +helpension.value;
-  animate();
-  
-}
-
-
-function modifyInputs() {
-
-         slider.oninput = modifyOffset;
-
-         const event = new Event("input");
-         slider.addEventListener(
-          "input",
-          (e) => {
-          },
-          false
-          );
-          slider.dispatchEvent(event);
-
-}
+    newPoint = (this.value - this.getAttribute("min")) / (this.getAttribute("max") - this.getAttribute("min"));
+    // was originally -1, changed to -5 to push starting point to left
+    offset   = -5;
+    if (newPoint < 0) { newPlace = 0;  }
+    else if (newPoint > 1) { newPlace = width; }
+    else { newPlace = width * newPoint + offset; offset -= newPoint;}
+    siblings = this.parentNode.childNodes;
+    for (var i = 0; i < siblings.length; i++) {
+        sibling = siblings[i];
+        if (sibling.id == this.id) { k = true; }
+        if ((k == true) && (sibling.nodeName == "OUTPUT")) {
+            outputTag = sibling;
+        }
+    }
+    outputTag.style.left       = newPlace + "px";
+    outputTag.style.marginLeft = offset + "%";
+    outputTag.innerHTML        = slider.value + "%";
 
 
-semer.addEventListener("click", function() {
-  if (popup.style.display === "block") {
-      popup.style.display = "none";
-      semer.innerHTML = "Om uträkningen";
-  }
-  else {
-    popup.style.display = "block";
-    semer.innerHTML = "Dölj kommentaren";
-  }
+    helpension.value = siffror[this.value].Premiepension + siffror[this.value].Inkomstpension + siffror[this.value].Garantipension + siffror[this.value].Tjänstepension;
+    deltidslon.value = (26800 * this.value) / 100;
+    procent.value = Math.round((helpension.value/deltidslon.value)*100) + "%";
+    output.innerHTML = slider.value + "%";
 
-//Give "x" in popup closing function
-close.addEventListener("click", function() {
-  popup.style.display = "none";
-  semer.innerHTML = "Om uträkningen";
-
-  // overlay.style.display = "none";
-})
-
-informHeight();
-
-} )
-
-//Below is all the code for the bar chart
+    // Update the data objecgt
+    data[0].Value = +deltidslon.value;
+    data[1].Value = +helpension.value;
+    animate();
+    
+    }
 
 
-//setting local variables, used on y-axis
-locale = d3.formatLocale({
-  decimal: ",",
-  thousands: "\u00a0",
-  grouping: [3],
-  currency: ["", ""],
-  minus: "\u2212",
-  percent: "\u202f%"
-})
+    function modifyInputs() {
+
+            slider.oninput = modifyOffset;
+
+            const event = new Event("input");
+            slider.addEventListener(
+            "input",
+            (e) => {
+            },
+            false
+            );
+            slider.dispatchEvent(event);
+
+    }
 
 
-function makeBarChart() {
+    semer.addEventListener("click", function() {
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+        semer.innerHTML = "Om uträkningen";
+    }
+    else {
+        popup.style.display = "block";
+        semer.innerHTML = "Dölj kommentaren";
+    }
 
-  // set the dimensions and margins of the graph
-  var margin = {top: 10, right: 30, bottom: 40, left: 60},
-  width = diagramdiv.offsetWidth - margin.left - margin.right,
-  // width = 350 - margin.left - margin.right,
+    //Give "x" in popup closing function
+    close.addEventListener("click", function() {
+    popup.style.display = "none";
+    semer.innerHTML = "Om uträkningen";
 
-  height = 350 - margin.top - margin.bottom;
+    // overlay.style.display = "none";
+    })
+
+    informHeight();
+
+    } )
+
+    //Below is all the code for the bar chart
+
+
+    //setting local variables, used on y-axis
+    locale = d3.formatLocale({
+    decimal: ",",
+    thousands: "\u00a0",
+    grouping: [3],
+    currency: ["", ""],
+    minus: "\u2212",
+    percent: "\u202f%"
+    })
 
 
 
-// append the svg object to the body of the page
-var svg = d3.select("#diagramdiv")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 40, left: 60},
+    width = diagramdiv.offsetWidth - margin.left - margin.right,
+    // width = 350 - margin.left - margin.right,
+
+    height = 350 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#diagramdiv")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
 
 
-// })
-// Parse the Data
-// d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
-// d3.json(data, function(data) {
+    // })
+    // Parse the Data
+    // d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+    // d3.json(data, function(data) {
 
 
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Inkomstslag; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .attr("class", "axisfat")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    // .attr("transform", "translate(-10,0)rotate(-45)")
-    .attr("transform", "translate(-10,0)")
-    .style("text-anchor", "center");
+    // X axis
+    var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(function(d) { return d.Inkomstslag; }))
+    .padding(0.2);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .attr("class", "axisfat")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+        // .attr("transform", "translate(-10,0)rotate(-45)")
+        .attr("transform", "translate(-10,0)")
+        .style("text-anchor", "center");
 
-  // Add Y axis
-  var y = d3.scaleLinear()
-  .domain([0, 27000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y)
-    .ticks(4)
-    // .tickFormat(d3.format(".2s")))
-    //to make the thousands separator a space instead of a comma
-    .tickFormat(locale.format('$,.0f')))
+    // Add Y axis
+    var y = d3.scaleLinear()
+    .domain([0, 27000])
+    .range([ height, 0]);
+    svg.append("g")
+    .call(d3.axisLeft(y)
+        .ticks(4)
+        // .tickFormat(d3.format(".2s")))
+        //to make the thousands separator a space instead of a comma
+        .tickFormat(locale.format('$,.0f')))
 
-  .attr("class", "axis")
+    .attr("class", "axis")
 
-// Create a variable for the bars. They need to be placed inside a "g" element, because text labels cannot be placed inside "rect" elements.
-var bars = svg.selectAll("mybar")
-  .data(data)
-  .enter().append("g");
-
-
-var xbandwidth = x.bandwidth();
+    // Create a variable for the bars. They need to be placed inside a "g" element, because text labels cannot be placed inside "rect" elements.
+    var bars = svg.selectAll("mybar")
+    .data(data)
+    .enter().append("g");
 
 
-bars.append("rect")
-  .attr("x", function(d) { return x(d.Inkomstslag); })
-  .attr("width", x.bandwidth())
-  .attr("fill", "#69b3a2")
-  .attr("y", function(d) { return y(d.Value); })
-  .attr("height", function(d) { return height - y(d.Value); })
-  //append the labels
-  bars.append("text")
-
-  .text(function(d,i) {     
-  })
-  .attr("class", "label")
-  .style("text-anchor", "left")
-  //place horisontally where d.Inkomstslag is the placement of the rect and xbandwidth is a variable with av value of halt the width of the rect. Then subtract about half the width of the text element, which varies a bit. Makes the label roughly centered on different screen sizes.
-  .attr("x", function(d) { return x(d.Inkomstslag) + xbandwidth/2 -27; })
-  //place vertically
-  .attr("y", height - 15)
+    var xbandwidth = x.bandwidth();
 
 
-
-function animate(d,i) {
-  // data[0].Value = data[0].Value * Math.random()
-  // Animation
-  bars.selectAll("rect")
-    .transition()
-    .duration(400)
+    bars.append("rect")
+    .attr("x", function(d) { return x(d.Inkomstslag); })
+    .attr("width", x.bandwidth())
+    .attr("fill", "#69b3a2")
     .attr("y", function(d) { return y(d.Value); })
     .attr("height", function(d) { return height - y(d.Value); })
-    .delay(function(d,i){ return(i*100)})
+    //append the labels
+    bars.append("text")
 
-
-  bars.select("text")
-    
-    //Update label text, format with thousands separator space and return
-    .text(function(d,i) { 
-      return locale.format('$,.0f') (data[i].Value);
+    .text(function(d,i) {     
     })
-    // .attr("class", "label")
-    // .style("text-anchor", "center")
-    // //place horisontally
-    // .attr("x", function(d) { return x(d.Inkomstslag) + 20; })
-    // //place vertically
-    // .attr("y", height - 15)
+    .attr("class", "label")
+    .style("text-anchor", "left")
+    //place horisontally where d.Inkomstslag is the placement of the rect and xbandwidth is a variable with av value of halt the width of the rect. Then subtract about half the width of the text element, which varies a bit. Makes the label roughly centered on different screen sizes.
+    .attr("x", function(d) { return x(d.Inkomstslag) + xbandwidth/2 -27; })
+    //place vertically
+    .attr("y", height - 15)
+
+
+
+    function animate(d,i) {
+    // data[0].Value = data[0].Value * Math.random()
+    // Animation
+    bars.selectAll("rect")
+        .transition()
+        .duration(400)
+        .attr("y", function(d) { return y(d.Value); })
+        .attr("height", function(d) { return height - y(d.Value); })
+        .delay(function(d,i){ return(i*100)})
+
+
+    bars.select("text")
+        
+        //Update label text, format with thousands separator space and return
+        .text(function(d,i) { 
+        return locale.format('$,.0f') (data[i].Value);
+        })
+        // .attr("class", "label")
+        // .style("text-anchor", "center")
+        // //place horisontally
+        // .attr("x", function(d) { return x(d.Inkomstslag) + 20; })
+        // //place vertically
+        // .attr("y", height - 15)
+    }
+
+
+
+    modifyInputs();
+
+    informHeight();
+
+
 }
 
-}  
-
-makeBarChart();
-
-
-modifyInputs();
-
-informHeight();
-
-// })
-
+//Render slider and bar chart for the first time
+doAllStuff();
 
 // Change size of charts on resizeBy, from here: https://www.tutorialspoint.com/how-to-wait-resize-end-event-and-then-perform-an-action-using-javascript
 
 // function to execute JavaScript code after the window resize event completes
 function executeAfterResize() {
   document.getElementById("diagramdiv").innerHTML = '';
-  makeBarChart();
-  modifyInputs();
+  doAllStuff();
 }
 var timeId = null;
 window.addEventListener('resize', () => {
   clearTimeout(timeId);
   timeId = setTimeout(executeAfterResize, 300);
 });
+
+
+// })
 
 
 // Source code animated bar chart: https://d3-graph-gallery.com/graph/barplot_animation_start.html
