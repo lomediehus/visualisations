33b81597540a415d3cfda037ad99bab66fbe117d
elinsteenKA <elin.steen@ka.se>
2023-08-08 14:43:26 +0200

    Add files via upload

---
+++ scrolltest/six.js
@@ -0,0 +1,27 @@
+const fadeElements = document.querySelectorAll('.fade-element');
+const windowHeight = window.innerHeight;
+let firstElementVisible = false;
+
+function handleScroll() {
+  fadeElements.forEach((element, index) => {
+    const elementOffsetTop = element.offsetTop;
+    const distanceFromTop = elementOffsetTop - window.scrollY;
+
+    if (index === 0) {
+      // For the first element, make it fully visible before moving to the second
+      const opacity = Math.min(1, Math.max(0, 1 - distanceFromTop / (windowHeight - 100)));
+      element.style.opacity = opacity.toString();
+
+      if (opacity === 1 && !firstElementVisible) {
+        firstElementVisible = true;
+      }
+    } else if (index === 1 && firstElementVisible) {
+      // For the second element, start becoming visible after the first is fully visible
+      const opacity = Math.min(1, Math.max(0, 1 - (distanceFromTop - 30) / (windowHeight - 100)));
+      element.style.opacity = opacity.toString();
+    }
+  });
+}
+
+// Attach the "scroll" event listener
+window.addEventListener('scroll', handleScroll);
