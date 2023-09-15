a5255d1c15c7832b425ab5c1d3befbc80ef91722
elinsteenKA <elin.steen@ka.se>
2023-09-15 14:11:55 +0200

    Add files via upload

---
+++ barkarta/bar.js
@@ -0,0 +1,28 @@
+console.log("script is running")
+
+const content = document.getElementById('content');
+
+let host = window.location.host;
+if (host.includes("github")) {
+  document.querySelector("link[rel='shortcut icon']").href = "favicon2.ico";
+  console.log('den finns på github')
+}
+
+// var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([63.108, 16.345], 13);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+var circle = L.circle([51.508, -0.11], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(map);
+
+circle.bindPopup("I am a circle.");
