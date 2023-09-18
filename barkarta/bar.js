decb06f38d217c9067f647b40b4a9e3399d81b16
elinsteenKA <elin.steen@ka.se>
2023-09-18 11:09:36 +0200

    Add files via upload

--- barkarta/bar.js
+++ barkarta/bar.js
@@ -9,7 +9,7 @@ if (host.includes("github")) {
 }
 
 // var map = L.map('map').setView([51.505, -0.09], 13);
-var map = L.map('map').setView([63.108, 16.345], 13);
+var map = L.map('map').setView([63.108, 15.345], 7);
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,11 +18,213 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-var circle = L.circle([51.508, -0.11], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 500
+let cirkel_color = "red";
+let cirkel_fillColor = "red";
+let cirkel_fillOpacity = 1;
+let cirkel_radius = 1000;
+
+
+var loos = L.circle([61.756, 15.173], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
 }).addTo(map);
 
-circle.bindPopup("I am a circle.");
+loos.bindPopup("Loos");
+
+
+var tandsjoborg = L.circle([61.710, 14.716], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+tandsjoborg.bindPopup("Tandsjöborg");
+
+var ljusdal = L.circle([61.829, 16.108], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+ljusdal.bindPopup("Ljusdal");
+
+
+var alvros = L.circle([62.046, 14.654], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+alvros.bindPopup("Älvros");
+
+
+var naggen = L.circle([62.277, 15.994], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+naggen.bindPopup("Naggen");
+
+
+var kolsillre = L.circle([62.398, 15.208], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+kolsillre.bindPopup("Kölsillre");
+
+var ytterhogdal = L.circle([62.175, 14.941], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+ytterhogdal.bindPopup("Ytterhogdal");
+
+
+var ange = L.circle([62.524, 15.654], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+ange.bindPopup("Ånge");
+
+
+var svenstavik = L.circle([62.766, 14.434], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+svenstavik.bindPopup("Svenstavik");
+
+var ostavall = L.circle([62.425, 15.478], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+ostavall.bindPopup("Östavall");
+
+var kalarne = L.circle([62.981, 16.089], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+kalarne.bindPopup("Kälarne");
+
+
+var bispgarden = L.circle([63.027, 16.624], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+bispgarden.bindPopup("Bispgården");
+
+
+var hammarstrand = L.circle([63.112, 16.344], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+hammarstrand.bindPopup("Hammarstrand");
+
+
+var ullanger = L.circle([63.012, 18.185], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+ullanger.bindPopup("Ullånger");
+
+
+var solleftea = L.circle([63.172, 17.249], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+solleftea.bindPopup("Sollefteå");
+
+var borgvattnet = L.circle([63.424, 15.823], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+borgvattnet.bindPopup("Borgvattnet");
+
+
+var hammerdal = L.circle([63.591, 15.355], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+hammerdal.bindPopup("Hammerdal");
+
+
+var hoting = L.circle([64.114, 16.203], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+hoting.bindPopup("Hoting");
+
+
+var junsele = L.circle([63.696, 16.878], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+junsele.bindPopup("Junsele");
+
+var rosson = L.circle([63.921, 16.339], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+rosson.bindPopup("Rossön");
+
+63.81127695983636, 16.40767115220841
+
+var backe = L.circle([63.811, 16.407], {
+  color: cirkel_color,
+  fillColor: cirkel_fillColor,
+  fillOpacity: cirkel_fillOpacity,
+  radius: cirkel_radius
+}).addTo(map);
+
+backe.bindPopup("Backe");
+
