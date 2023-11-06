075625536219f4b9ffc311712f3c7c466aa960ba
elinsteenKA <elin.steen@ka.se>
2023-11-06 13:38:25 +0100

    Add files via upload

---
+++ julkalender/julkalender.js
@@ -0,0 +1,9 @@
+//make it possible to console log with c(tobelogged)
+const c = console.log.bind(document);
+
+//Get one favicon for localhost and another for github pages
+let host = window.location.host;
+if (host.includes("github")) {
+  document.querySelector("link[rel='shortcut icon']").href = "favicon2.ico";
+}
+
