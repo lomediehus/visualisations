a81d5bdc814e4d15458f78065e9bf3517dc8f7cc
elinsteenKA <elin.steen@ka.se>
2024-01-09 15:37:50 +0100

    Add files via upload

--- framgang/framgang.js
+++ framgang/framgang.js
@@ -4,6 +4,7 @@ console.log("Kör testskript")
 
 //Create a tooltip, hidden at the start
 var tooltip = d3.select("body").append("div").attr("class","tooltip u-textMeta");
+var cirkeldata;
 
 
 //function for clicking the radiobuttons to answer the questions
@@ -49,14 +50,14 @@ function showTooltip(d) {
   
 
 function clicked(d,i) {
-   
+  console.log(d3.event)
+
 
   // Testar att placera vid klicket
 
     var x = d3.event.pageX - 50;
-    var y = d3.event.pageY - 100;
+    var y = d3.event.pageY;
 
-    console.log(y)
 
     kartpopup.style.left = x + "px";
     kartpopup.style.top = y + "px";
@@ -67,7 +68,7 @@ function clicked(d,i) {
   
     let markup = `
       <div id="kartpopuprubbe" class="u-textMeta fet">${d.ort}</div>
-      <div id="kartpopuptext" class="u-textMeta">${d.rubrik}</div>
+      <div id="kartpopuptext" class="u-textMeta"><img src="${d.bildurl}">${d.rubrik}</div>
       ${(() => {
         if (d.url != "") {
           return `
@@ -93,7 +94,10 @@ function clicked(d,i) {
     //   close();
         kartpopup.style.display = "none";
         overlay.style.display = "none";
+        d3.select("#loader").classed("spin", false)
+
     })
+
   
   }
 
@@ -107,14 +111,13 @@ document.addEventListener("DOMContentLoaded", function() {
         .center([16.382656313727672,62.34103687152436]) //projection center
         .parallels([55.327583999999995,69.059967]) //parallels for conic projection
         .rotate([343.6173436862723]) //rotation for conic projection
-        .translate([w*1.4, h/2.4]) //translate to center the map in view;
+        .translate([w*1.46, h/2.4]) //translate to center the map in view;
 
     var bana = d3.geoPath().projection(projection);
     var map = d3.json("sverige.geojson");
 
     Promise.all([map]).then(function(values) {
 
-        console.log(values)
         svg.selectAll("path")
             .data(values[0].features)
             .enter()
@@ -131,7 +134,26 @@ document.addEventListener("DOMContentLoaded", function() {
         //Nesting the circle-drawing to ensure they are drawn after the map is drawn.
         var datafile = d3.json("framgang.json");
         Promise.all([datafile]).then(function(values) {
-            console.log(values)
+            cirkeldata = values[0];
+
+            //get img urls to preload them
+            const imageUrls = [];            
+            cirkeldata.forEach(function(item, index){
+              console.log(item.bildurl)
+              imageUrls.push(item.bildurl)
+            })
+
+            function preloadImages(urls) {
+              urls.forEach(url => {
+                  const img = new Image();  // Create new Image object
+                  img.src = url;            // Set the src to the image URL
+              });
+            }
+          
+            // Call the function to preload the images when your page loads
+            window.onload = function() {
+                preloadImages(imageUrls);
+            };
 
 
             svg
@@ -142,21 +164,6 @@ document.addEventListener("DOMContentLoaded", function() {
             .attr("cx", function(d){ return projection([d.long, d.lat])[0] })
             .attr("cy", function(d){ return projection([d.long, d.lat])[1] })
             .attr("r", 5)
-            // .attr("class", function(d) { return d.kategori === "arbetsvillkor" ? "cirkel greenCircle" : "cirkel yellowCircle"; })
-            // .style("fill", "#e00f00")
-            // .style("fill", function(d){
-              
-            //   switch (d.kategori) {
-            //     case "arbetskläder":
-            //       return "green";
-            //     case "arbetsvillkor":
-            //       return "yellow";
-            //     case "språkproblem":
-            //       return "red";
-            //   }
-            
-      
-            // })
             .attr("class", function(d){
 
               switch (d.kategori) {
@@ -191,11 +198,11 @@ document.addEventListener("DOMContentLoaded", function() {
             // .attr("class", "cirkel")
             .on("click",clicked)
             .on("mouseover", function(d) {
-            d3.select(this).attr("r", 5).style("fill", "white");
+            // d3.select(this).attr("r", 5).style("fill", "white");
             showTooltip(d)
             })
             .on("mouseout", function(d) {
-            d3.select(this).attr("r", 5).style("fill", "#d00f00");
+            // d3.select(this).attr("r", 5).style("fill", "#d00f00");
             hideTooltip()
             });
 
@@ -218,31 +225,69 @@ document.addEventListener("DOMContentLoaded", function() {
 
             })
 
-            // Give radiobuttons a value dynamically and add click event
-            // d3.selectAll(".checkbox").each(function(d,i){
-            //   var value = "kat" + (i+1);
-            //   d3.select(this).on("click", function(){
-            //     console.log(value)
-            //   })              
-            // })
+            d3.select("#loader").on("click", function() {
+              let slumpsiffra = Math.floor(Math.random()*cirkeldata.length)
 
-            d3.select("#toggleButton").on("click", function() {
-              console.log(this.value)
-              // Check if the yellow circles are currently visible
-              var yellowCirclesVisible = d3.selectAll(".yellowCircle").style("visibility") === "visible";
+              
+              this.classList.add("spin")
+
+              // Capture the d3.event outside the timer
+              console.log(d3.event.pageX + '   ' + d3.event.pageY);
+              console.log(cirkeldata[slumpsiffra])
+
+    
+              setTimeout(function() {
+                clickedSpinner(cirkeldata[slumpsiffra]);
+            }, 2000);
+
+
+          function clickedSpinner(d,i) {
+          
+            //manuell position eftersom jag inte kan få klick-position in i timer-funktionen
+            var x = 150;
+            var y = 350;
+            kartpopup.style.left = x + "px";
+            kartpopup.style.top = y + "px";
+            kartpopup.style.display = "block";
+            overlay.style.display = "block";
+          
+            let markup = `
+              <div id="kartpopuprubbe" class="u-textMeta fet">${d.ort}</div>
+              <div id="kartpopuptext" class="u-textMeta"><img src="${d.bildurl}">${d.rubrik}</div>
+              ${(() => {
+                if (d.url != "") {
+                  return `
+                  <a id="kartpopuplink" class="u-textMeta red" target="blank" href="${d.url}">Läs mer här!</a>
+                  `
+                }
+                else {
+                  return`
+                  `
+                }
+              })()}
+              <div class="close"><img id="closex" class="closex" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2076398/closex.png"></img></div>
+              `
             
-              // Toggle visibility based on the current state
-              d3.selectAll(".yellowCircle").style("visibility", yellowCirclesVisible ? "hidden" : "visible");
+              kartpopup.innerHTML = markup;
+          
+              overlay.addEventListener("click", function(){
+                  kartpopup.style.display = "none";
+                  overlay.style.display = "none";
+          
+              })
+              closex.addEventListener("click", function() {
+              //   close();
+                  kartpopup.style.display = "none";
+                  overlay.style.display = "none";
+                  d3.select("#loader").classed("spin", false)          
+              })
+                      
+            }
+              
             }); 
 
       })
      
-
-      
-             
-   
-
-
 
 });
 
