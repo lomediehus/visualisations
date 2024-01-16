b7281139d466cf1a5af9b567be0d77ae90b1dcad
elinsteenKA <elin.steen@ka.se>
2024-01-16 15:13:49 +0100

    Update framgang.js

--- framgang/framgang.js
+++ framgang/framgang.js
@@ -3,7 +3,7 @@ console.log("Kör testskript")
 //make it possible to console log with c(tobelogged)
 const c = console.log.bind(document);
 
-Get one favicon for localhost and another for github pages
+//Get one favicon for localhost and another for github pages
 let host = window.location.host;
 if (host.includes("github")) {
   document.querySelector("link[rel='shortcut icon']").href = "favicon2.ico";
