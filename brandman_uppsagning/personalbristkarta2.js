0e60451bf118eb486d531986c4f387eb716eaea9
elinsteenKA <elin.steen@ka.se>
2024-03-27 10:25:13 +0100

    Add files via upload

---
+++ brandman_uppsagning/personalbristkarta2.js
@@ -0,0 +1,174 @@
+//make it possible to console log with c(tobelogged)
+const c = console.log.bind(document);
+
+//Get one favicon for localhost and another for github pages
+let host = window.location.host;
+if (host.includes("github")) {
+  document.querySelector("link[rel='shortcut icon']").href = "favicon2.ico";
+  console.log('den finns på github')
+}
+
+//Variables for map size and projection
+var w = 264,
+		h = 500,
+		legendRectSize = 18,
+		legendSpacing = 4,
+		tooltip;
+
+var svg = d3.select('div#chart').append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 " + w + " " + h).classed("svg-content", true);
+var projection = d3.geoConicEqualArea()
+    .scale(1800)
+    .center([16.382656313727672,62.34103687152436]) //projection center
+    .parallels([55.327583999999995,69.059967]) //parallels for conic projection
+    .rotate([343.6173436862723]) //rotation for conic projection
+    .translate([w*1.3, h/2.1]) //translate to center the map in view;
+
+var thepath = d3.geoPath().projection(projection);
+
+//load the geodata file
+d3.json("SverigesKommuner.geojson").then(function(geodata){
+	//load a json data file
+	d3.json("kommundata.json").then(function(artikeldata){
+		//make a new array out of the feature part of the geodata
+		let kombo =[...geodata.features];
+		//loop the new array of geodata
+		kombo.forEach(function(kombogrej) {
+			//filter array by a common item ( do a lookup ). "Result" will be an array with one item, which will be an object containing all the text from the json file.
+			var result = artikeldata.filter(function(regiondata) {
+				return regiondata.Kod.toString() === kombogrej.properties.KNKOD.toString();
+			})
+
+			// Create new items in the array. Value pairs from "result" are stored with new keys in the "kombo" array, which will now contain both the geodata and the selected data from the json file.
+			kombogrej["Kommun"] = (result[0] !== undefined) ? result[0].Kommun : null;
+			kombogrej["Övertid2022"] = (result[0] !==undefined) ? result[0].Övertid2022 : null;
+			kombogrej["Ökat?"] = (result[0] !==undefined) ? result[0]["Ökat?"] : null;
+      kombogrej["Ökning_pga_personalbrist"] = (result[0] !==undefined) ? result[0].Ökning_pga_personalbrist : null;
+      kombogrej["Personalbrist?"] = (result[0] !==undefined) ? result[0]["Personalbrist?"] : null;
+		})
+
+		svg.selectAll("path")
+				//using the "kombo" array to create map
+	      .data(kombo)
+	      .enter()
+	      .append("path")
+	      // .attr("class", "continent")
+				.attr("class", quantify)
+	      .attr("d", thepath)
+					.style("stroke", "black")
+					.style("stroke-width", 0.5)
+						.on("mouseover", mouseover)
+						.on("mousemove", mousemove)
+
+						.on("mouseout", mouseout);
+
+		//color and text for the legend. (Addding an extra item to the domain and range arrays will create a new legend item)
+		var color = d3.scaleOrdinal()
+				.domain(["Personalbrist", "Ej personalbrist", "Vet ej/Ej svar"])
+        // .range(["#", "#009E73", "#d8d8d8"]);
+        .range(["#009E73", "#56B4E9", "#d8d8d8"]);
+
+
+
+		var legend = d3.select("svg")
+				.append("g")
+				.selectAll("g")
+				.data(color.domain())
+				.enter()
+				.append("g")
+					.attr("class", "legend")
+					.attr("transform", function(d,i) {
+						var height = legendRectSize;
+						var horz = 0;
+						var vert = i * height;
+						return "translate(" + horz + "," + vert + ")";
+					})
+
+		legend.append("rect")
+				.attr("width", legendRectSize)
+				.attr("height", legendRectSize)
+				.style("fill", color)
+				.style("stroke", color);
+
+
+		legend.append("text")
+			.attr("x", legendRectSize + legendSpacing)
+			.attr("y", legendRectSize - legendSpacing)
+			.attr("class", "u-textMeta legend")
+			.text(function(d) {return d; });
+
+		tooltip = d3.select("section")
+				.append("div")
+					.attr("class", "tooltip u-textMeta")
+					.style("opacity", 0)
+
+			//Set up colors for the map regions using data from the "kombo" array. Returns a class name. The colors are defined in the css file
+		function quantify(d,i) {
+			var f;
+
+      switch(d["Personalbrist?"]) {
+        case "Ja":
+        f = 4;
+        break;
+        case "Nej":
+        f = 5;
+        break;
+        default:
+        f = 3;
+      }
+        //left code that is not used this time, as a reminder that you can set the classes by a math calculation
+			// return "q" + Math.min(8, ~~((f-250) / 150)) + "-9";
+			return "q" + f;
+
+
+			}
+	})
+
+	//hide the loader when the map has been drawn
+	document.getElementById('loader').style.display = "none";
+
+  //Set the iframe height, using function in external lomediehus script
+  informHeight();
+
+})
+
+function mouseover() {
+	tooltip.transition()
+		.duration(200)
+		.style("opacity", .9);
+}
+
+function mousemove(d, i) {
+	let markup = `
+		<p class='fet no-margin-bottom'>${d.Kommun}</p>
+		<p class='no-margin-bottom'> ${d["Personalbrist?"] === "Ja" ? "Personalbrist" : ""}</p>
+	`
+
+	tooltip.html(markup)
+			// .style("left", (d3.event.pageX) + "px")
+			.style("left", function() {
+
+				//on a small screen, place tooltip further to the left on the right side of the screen (making tooltips wider)
+				if (d3.event.pageX > window.innerWidth/2) {
+					return (d3.event.pageX - 100) + "px";
+					}
+				else {
+					return (d3.event.pageX) + "px";
+				}
+			})
+
+			.style("top", (d3.event.pageY - 50) + "px");
+}
+
+function mouseout() {
+		tooltip.transition()
+			.duration(200)
+			.style("opacity", 0);
+}
+
+
+//set zoom, code remains as a reminder
+// svg.call(d3.zoom().on("zoom", function () {
+//        svg.attr("transform", d3.event.transform)
+// 		 }))
+		 //don't zoom on scroll wheel (only touch)
+		 // .on("wheel.zoom", null);
