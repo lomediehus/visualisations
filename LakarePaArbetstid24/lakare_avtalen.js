6d1c806686b728d8684222209e53e1680ba898cc
elinsteenKA <elin.steen@ka.se>
2024-04-04 15:20:43 +0200

    Add files via upload

---
+++ LakarePaArbetstid24/lakare_avtalen.js
@@ -0,0 +1,166 @@
+var avtalen = [
+  avtal1 = {
+    bransch: 1,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Kommun- och region&shy;anställda (undersköterskor, vårdbiträden med flera)</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med SKR, Sveriges Kommuner och Regioner</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare eller tandläkare vid akut sjukdom eller olycksfall.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök: </span>Arbetsgivaren kan även ge ledigt med lön för andra besök i hälso- och sjukvården.</p>",
+
+
+    yrke: ["aktiveringsassistent", "aktiveringspedagog", "aktivitetsledare", "arbetshandledare", "badmästare", "badpersonal", "behandlare", "behandlingsassistent", "boendeassistent", "boendehandledare", "boendestödjare", "brandman", "habiliteringsassistent", "ekonomibiträde", "habiliteringsbiträde", "habiliteringspedagog", "habiliteringspersonal", "obduktionstekniker", "omsorgsassistent", "socialpedagog", "stödassistent", "stödbiträde", "stödpedagog", "stödpersonal", "tvätteribiträde", "tvättmaskinskötare", "ambulanssjukvårdare", "anläggningsarbetare", "barnskötare", "barnsköterska", "biträden", "elevassistent", "fastighetsskötare", "fordonsförare", "fotvårdare", "fritidsledare", "förrådsarbetare", "kock", "kokerska", "laboratoriebiträde", "lokalvårdare", "måltidspersonal", "park- och trädgårdsarbetare", "personlig assistent", "skötare", "städare", "undersköterska", "vaktmästare", "vårdare", "vårdbiträde"]
+  },
+
+  avtal2 = {
+    bransch: 2,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i vårdbolag som drivs av kommuner, regioner, stiftelser, föreningar och privata företag (undersköterskor, vårdbiträden med flera)</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sobona</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare eller tandläkare vid akut sjukdom eller olycksfall.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök: </span>Arbetsgivaren kan även ge ledigt med lön för andra besök i hälso- och sjukvården.</p>",
+
+
+    yrke: ["aktiveringsassistent", "aktiveringspedagog", "aktivitetsledare", "arbetshandledare", "badmästare", "badpersonal", "behandlare", "behandlingsassistent", "boendeassistent", "boendehandledare", "boendestödjare", "brandman", "habiliteringsassistent", "ekonomibiträde", "kock", "kokerska", "habiliteringsbiträde", "habiliteringspedagog", "habiliteringspersonal", "handledare", "obduktionstekniker", "omsorgsassistent", "socialpedagog", "stödassistent", "stödbiträde", "stödpedagog", "stödpersonal", "tvätteribiträde", "tvättmaskinskötare", "ambulanssjukvårdare", "anläggningsarbetare", "barnskötare", "barnsköterska", "biträden", "elevassistent", "fastighetsskötare", "fordonsförare", "fotvårdare", "fritidsledare", "förrådsarbetare", "kock", "kokerska", "laboratoriebiträde", "lokalvårdare", "måltidspersonal", "park- och trädgårdsarbetare", "personlig assistent", "skötare", "städare", "undersköterska", "ungdomsassistent", "vaktmästare", "vårdare", "vårdbiträde"]
+  },
+
+
+  avtal3 = {
+    bransch: 3,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i privat vård</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna, Bransch vård, behandlings- samt omsorgsverksamhet (E)</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Regler saknas i avtalet.</p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Regler saknas i avtalet.</p>",
+
+    yrke: ["undersköterska", "vårdbiträde", "kökspersonal", "boendestödjare", "vårdare", "skötare", "lokalvårdare", "fastighetsskötare", "stödassistent"]
+  },
+
+  avtal4 = {
+    bransch: 3,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna, Bransch äldreomsorg (F)</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Regler saknas i avtalet.</p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Regler saknas i avtalet.</p>",
+
+    yrke: ["undersköterska", "vårdbiträde", "kökspersonal", "lokalvårdare", "fastighetsskötare"]
+  },
+
+
+  avtal5 = {
+    bransch: 3,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Fremia, Hälsa, vård och övrig omsorg</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Regler saknas i avtalet.</p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Regler saknas i avtalet.</p>",
+
+    yrke: ["undersköterska", "vårdbiträde", "behandlingsassistent", "socialpedagog", "logopedassistent", "sjukgymnastassistent", "medlevare", "vård- och omsorgspersonal", "lokalvårdare", "köksbiträde", "vaktmästare"]
+  },
+
+  avtal6 = {
+    bransch: 3,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Arbetsgivaralliansen, Vård- och omsorg</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Ej reglerat.</p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Besök på barnmorskemottagning för båda föräldrarna kan få ske på arbetstid när verksamheten så tillåter.</p>",
+
+
+    yrke: ["undersköterska", "vårdbiträde", "behandlingsassistent", "socialpedagog", "vårdare", "medlevare", "fältarbetare", "sjukgymnastassistent", "vård- och omsorgspersonal", "lokalvårdare" , "kokerska", "kock", "vaktmästare", "ungdomsledare", "fritidsledare", "butikspersonal"]
+  },
+
+  avtal7 = {
+    bransch: 4,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i Svenska Kyrkan</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Svenska Kyrkan</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare eller tandläkare vid akut sjukdom eller olycksfall.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Vid efterbehandlingar med remiss. Arbetsgivaren kan även ge ledigt med lön för andra besök inom hälso- och sjukvården, exempelvis blodgivning.</p>",
+
+    yrke: ["assistent/församlingsarbete", "fritidsledare", "ungdomsledare", "barntimmeledare", "barnskötare", "diakoniassistent", "arbetsledare", "kyrkvaktmästare", "kyrkogårdsarbetare", "församlingshemsvärd", "husmor", "städare", "fastighetsskötare", "ekonomibiträde", "kontorsvaktmästare", "kyrkogårdsföreståndare", "lokalvårdare"]
+  },
+
+  avtal8 = {
+    bransch: 5,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Personliga assistenter, ej kommunanställda</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Vårdföretagarna</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Regler saknas i avtalet.</p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Regler saknas i avtalet.</p>",
+
+
+    yrke: ["personlig assistent"]
+  },
+
+  avtal9 = {
+    bransch: 5,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Fremia</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Regler saknas i avtalet.</p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Regler saknas i avtalet.</p>",
+
+
+    yrke: ["personlig assistent"]
+  },
+
+  avtal10 = {
+    bransch: 5,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sobona</h3>",
+    avtalstext1: "<p>(PAN-avtalet ersätter från 1 april 2024 Sobonas assistansavtal, fd KFS.)<br><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Regler saknas i avtalet.</span></p><p><span class='u-textStrong'>Ledighet vid planerade besök:</span> Regler saknas i avtalet.</p>",
+
+
+    yrke: ["personlig assistent"]
+  },
+
+  
+  avtal11 = {
+    bransch: 6,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Bussförare</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sveriges Bussföretag</h3>",
+    avtalstext1:  "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare eller tandläkare vid akut sjukdom eller olycksfall.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Vid förstagångsbesök vid sjukvårdsinrättning efter remiss av företagsläkare eller av arbetsgivaren godkänd läkare. För hälsokontroll som ordnats av företaget eller lagstadgad hälsoundersökning för giltigt körkort.</p>",
+
+    yrke: ["bussförare", "spårvagnsförare", "verkstadspersonal", "mekaniker", "motorfordonsmekaniker", "reparatör", "lokalvårdare" , "garagepersonal", "buss övriga", "chaufför"]
+  },
+
+  avtal12 = {
+    bransch: 6,
+    avtalsoverskrift: "",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Sobona</h3>",
+    avtalstext1:  "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare eller tandläkare vid akut sjukdom eller olycksfall.<br>Arbetsgivaren kan även i övrigt bevilja ledighet med lön för besök inom hälso- och sjukvården.<br>Blivande föräldrar kan under havandeskap göra två besök på mödravårdcentral utan löneavdrag.</p>",
+
+    yrke: ["bussförare", "spårvagnsförare", "verkstadspersonal", "mekaniker", "motorfordonsmekaniker", "reparatör", "lokalvårdare" , "garagepersonal", "buss övriga", "chaufför"]
+  },
+
+  avtal13 = {
+    bransch: 7,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i jordbruk</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Gröna Arbetsgivare</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare och tandläkare vid akut sjukdom eller olycksfall.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Vid besök på sjukvårdsinrättning efter remiss av företagsläkare eller efter kallelse för mammografi, cellprov eller PSA-provtagning.</p>",
+
+    yrke: ["arbetsledare", "traktorförare", "maskinskötare", "maskinförare", "reparatör", "avbytare", "djurskötare", "lantarbetare"]
+  },
+
+  avtal14 = {
+    bransch: 8,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i djursjukvård</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Gröna Arbetsgivare</h3>",
+    avtalstext1:"<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare och tandläkare vid akut sjukdom eller olycksfall.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Vid besök på sjukvårdsinrättning efter remiss av företagsläkare eller efter kallelse för mammografi, cellprov eller PSA-provtagning.</p>",
+  
+  
+    yrke: ["djursjukskötare", "djurskötare", "djurvårdare"]
+  },
+
+  avtal15 = {
+    bransch: 9,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Samhallanställda</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Samhall</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid akut sjukdom, olycksfall eller akuta tandbesvär besöka läkare, sjukhus eller tandläkare. Besöket ska kunna styrkas.</p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Det gäller även efterbehandlingar, röntgen, skärmbildsundersökning. Vid mammografi, cell- och PSA-provtagning. Besöket ska kunna styrkas.</p>",
+
+    yrke: ["administratör", "chaufför", "driftsledare", "fastighetsskötare", "fönsterputsare", "instruktör", "internlärare", "kundtjänstmedarbetare", "lagerarbetare", "ledsagare", "lokalvårdare", "montör", "receptionist", "renhållningsarbetare", "omsorgsassistent", "serveringsbiträde", "servicevärd", "städare", "teamledare", "tvätterimedarbetare", "vaktmästare"]
+  },
+
+  avtal16 = {
+    bransch: 10,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Privatanställda städare</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Almega Tjänsteförbunden, serviceentreprenad</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Vid förstagångsbesök hos läkare och tandläkare vid akut sjukdom eller olycksfall. </p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Besök på sjukvårdsinrättning efter remiss av företagsläkare.</p>",
+
+
+    yrke: ["städare", "lokalvårdare"]
+  },
+  avtal17 = {
+    bransch: 11,
+    avtalsoverskrift: "<p class=\"u- u-spacingTopM u-textMetaDeca caps\">Anställda i storkök</p>",
+    avtalsrubrik: "<h3 class=\"u-textMetaDeca\">Avtalet med Visita</h3>",
+    avtalstext1: "<p><span class='u-textStrong'>Ledighet med lön vid akut sjukdom:</span> Förstagångsbesök hos läkare och tandläkare vid akut sjukdom eller olycksfall. </p><p><span class='u-textStrong'>Ledighet med lön vid planerade besök:</span> Besök vid sjukvårdsinrättning efter remiss av företagsläkare eller annan av arbetsgivaren anvisad läkare.</p>",
+
+
+    yrke: ["kock", "måltidspersonal", "köksbiträde", "ekonomibiträde"]
+  }
+
+]
