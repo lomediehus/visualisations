a9637be5f57be4bfb6025c8ca6d9ea7b0a490a0c
elinsteenKA <elin.steen@ka.se>
2024-04-15 14:26:08 +0200

    Add files via upload

--- scrolltrigger_gsap/script.js
+++ scrolltrigger_gsap/script.js
@@ -1,13 +1,57 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".rect_right", {opacity:1, x:200, scrollTrigger:{
-  trigger: ".scroller",
-  markers:true,
-  start: "top 400",
-  // end: "top 25",
-  // events: onEnter onLeave onEnterBack onLeaveBack
-  toggleActions: "restart reverse reverse reverse"
-}
-})
+gsap.set(".pinwheel", {transformOrigin: "center center", xPercent: -50, x: 0, yPercent: -50, y: 0});
+
+gsap.to(".pinwheel", {
+  // opacity: 1,
+  rotation: 360,
+  ease: "none",
+  scrollTrigger: {
+    scroller: ".scroller",
+    trigger: ".pinwheel",
+    start: "center center",
+    end: "+=800",
+    pin: true,
+    scrub: 1
+  }
+});
+
+// gsap.set(".rect_right", {transformOrigin: "center center", xPercent: -50, x: 0, yPercent: -50, y: 0});
+
+gsap.to(".rect_right", {
+  opacity: 1,
+  // rotation: 360,
+  ease: "none",
+  scrollTrigger: {
+    scroller: ".scroller",
+    trigger: ".rect",
+    // start: "center center",
+    // end: "+=800",
+    start: 0,
+    end: 0,
+    // pin: true,
+    scrub: 1
+  }
+});
+
+
+// let comparison = document.querySelector(".comparison-section"),
+//     tl = gsap.timeline({
+// 			scrollTrigger: {
+//         scroller: ".scroller",
+// 				trigger: comparison,
+// 				start: "center center",
+//         // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+// 				end: () => "+=" + comparison.offsetWidth, 
+// 				scrub: true,
+// 				pin: true
+// 			},
+// 			defaults: {ease: "none"}
+// 		});
+// 	// animate the container one way...
+// 	tl.fromTo(".after-image", { xPercent: 100, x: 0}, {xPercent: 0})
+// 	  // ...and the image the opposite way (at the same time)
+// 	  .fromTo(".after-image img", {xPercent: -100, x: 0}, {xPercent: 0}, 0);
+
 
 informHeight();
