9eb3b58ea5147aa6066497749cee337e808560b6
elinsteenKA <elin.steen@ka.se>
2024-04-15 14:14:41 +0200

    Add files via upload

--- scrolltrigger_gsap/script.js
+++ scrolltrigger_gsap/script.js
@@ -1,38 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.set(".pinwheel", {transformOrigin: "center center", xPercent: -50, x: 0, yPercent: -50, y: 0});
-
-gsap.to(".pinwheel", {
-  rotation: 360,
-  ease: "none",
-  scrollTrigger: {
-    scroller: ".scroller",
-    trigger: ".pinwheel",
-    start: "center center",
-    end: "+=800",
-    pin: true,
-    scrub: 1
-  }
-});
-
-
-let comparison = document.querySelector(".comparison-section"),
-    tl = gsap.timeline({
-			scrollTrigger: {
-        scroller: ".scroller",
-				trigger: comparison,
-				start: "center center",
-        // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
-				end: () => "+=" + comparison.offsetWidth, 
-				scrub: true,
-				pin: true
-			},
-			defaults: {ease: "none"}
-		});
-	// animate the container one way...
-	tl.fromTo(".after-image", { xPercent: 100, x: 0}, {xPercent: 0})
-	  // ...and the image the opposite way (at the same time)
-	  .fromTo(".after-image img", {xPercent: -100, x: 0}, {xPercent: 0}, 0);
-
+gsap.to(".rect_right", {opacity:1, x:200, scrollTrigger:{
+  trigger: ".scroller",
+  markers:true,
+  start: "top 400",
+  // end: "top 25",
+  // events: onEnter onLeave onEnterBack onLeaveBack
+  toggleActions: "restart reverse reverse reverse"
+}
+})
 
 informHeight();
