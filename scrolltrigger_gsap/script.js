0ef33f178e1b95b836d197e575497c598649d6aa
elinsteenKA <elin.steen@ka.se>
2024-04-16 15:41:16 +0200

    Add files via upload

--- scrolltrigger_gsap/script.js
+++ scrolltrigger_gsap/script.js
@@ -1,20 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.set(".pinwheel", {transformOrigin: "center center", xPercent: -50, x: 0, yPercent: -50, y: 0});
+// gsap.set(".pinwheel", {transformOrigin: "center center", xPercent: -50, x: 0, yPercent: -50, y: 0});
 
-gsap.to(".pinwheel", {
-  // opacity: 1,
-  rotation: 360,
-  ease: "none",
-  scrollTrigger: {
-    scroller: ".scroller",
-    trigger: ".pinwheel",
-    start: "center center",
-    end: "+=800",
-    pin: true,
-    scrub: 1
-  }
-});
+// gsap.to(".pinwheel", {
+//   // opacity: 1,
+//   rotation: 360,
+//   ease: "none",
+//   scrollTrigger: {
+//     scroller: ".scroller",
+//     trigger: ".pinwheel",
+//     start: "center center",
+//     end: "+=800",
+//     pin: true,
+//     scrub: 1
+//   }
+// });
 
 // gsap.set(".rect_right", {transformOrigin: "center center", xPercent: -50, x: 0, yPercent: -50, y: 0});
 
