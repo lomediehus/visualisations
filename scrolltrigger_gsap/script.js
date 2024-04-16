240b8f46e0985869769b03565dc17d214a8139b2
elinsteenKA <elin.steen@ka.se>
2024-04-16 15:08:08 +0200

    Add files via upload

--- scrolltrigger_gsap/script.js
+++ scrolltrigger_gsap/script.js
@@ -1,11 +1,12 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".rect_right", {opacity:1, scrollTrigger:{
-  // scroller:".scroller",
-  trigger: ".scroller",
+  // scroller:".container",
+  trigger: ".rect_right",
   markers:true,
-  start: "top center",
-  end: "+=300",
+  start: "0 500",
+  end: "100 50",
+  // scrub:1,
   // pin:true
   // events: onEnter onLeave onEnterBack onLeaveBack
   // toggleActions: "restart pause reverse reset"
@@ -14,3 +15,4 @@ gsap.to(".rect_right", {opacity:1, scrollTrigger:{
 })
 
 
+informHeight();
