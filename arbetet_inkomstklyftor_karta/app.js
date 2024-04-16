3e07f585c9daca4735e362db401238c5be6f0869
Karl Martinsson <36486318+karlmartinsson@users.noreply.github.com>
2024-04-16 15:12:05 +0200

    Add files via upload

---
+++ arbetet_inkomstklyftor_karta/app.js
@@ -0,0 +1,27 @@
+let alla_kommuner = ["Stockholm", "Uppsala"];
+informHeight();
+let kommunhtml = document.getElementById("kommuner");
+
+alla_kommuner.forEach((kommun) => {
+    let para = document.createElement("a");
+    para.onclick = function () {
+        fetchData(kommun)
+    };
+    const node = document.createTextNode(kommun);
+    para.appendChild(node);
+    kommunhtml.appendChild(para);
+}
+)
+
+
+async function fetchData(kommun) {
+    let url = "https://lomediehus.github.io/visualisations/arbetet_karttest/" + kommun + ".json"
+    let x = await fetch(url);
+    let y = await x.text();
+    y = JSON.parse(y);
+    updateMap(y);
+    informHeight();
+  }
+
+
+//
