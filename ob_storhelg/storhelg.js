915fc04f466a19aaad13f58b528af1031402ec3d
elinsteenKA <elin.steen@ka.se>
2024-06-10 13:10:01 +0200

    Add files via upload

--- ob_storhelg/storhelg.js
+++ ob_storhelg/storhelg.js
@@ -145,7 +145,7 @@ function doStuff() {
   var paskdatum = new Date("February 21 2024");
   var pingstdatum = new Date("April 22 2023");
 
-  var midsommardatum = new Date("June 07 2023");
+  var midsommardatum = new Date("June 07 2024");
 
   //Variable for current date
   //variables for set dates
@@ -155,10 +155,10 @@ function doStuff() {
   if (date.getTime() > trettonhelgsdatum.getTime()) {
     c("efter trettonhelgen")
     //if current date is also after nyar
-    if (date.getTime() > paskdatum.getTime()) {
-      document.getElementById("helgbild").src = 'pask.svg';
-      goActive(helgknapp4);
-      helgnr = "4";
+    if (date.getTime() > midsommardatum.getTime()) {
+      document.getElementById("helgbild").src = 'midsommar.svg';
+      goActive(helgknapp6);
+      helgnr = "6";
     }
   //   else {
   //     document.getElementById("helgbild").src = 'nyar.svg';
