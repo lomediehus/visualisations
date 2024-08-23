9f13cdac14e729e4d0f04816e7c33c46e24e83d5
elinsteenKA <elin.steen@ka.se>
2024-08-23 10:44:06 +0200

    Add files via upload

--- ob_storhelg/storhelg.js
+++ ob_storhelg/storhelg.js
@@ -418,7 +418,8 @@ function doStuff() {
       //give error message
       else {
         messagediv.innerHTML ='';
-        var mailBody = 'Jag%20saknar%20yrkestiteln%20' + inpValue + '.';
+        var mailBody = 'Jag%20saknar%20yrkestiteln%20' + inpValue + '.%0A%0A(Mail från tjänsten OB Storhelg.)';
+
         node.innerHTML = '<p class="paddingTop10">Vi hittade inte yrket du sökte. Tycker du att yrket ska finnas med? Mejla oss yrkestiteln så kollar vi på det. Men Kommunal har så många yrken och avtal att vi tyvärr inte kan ha med alla.</p><a class="homestyled u-textMeta u-textStrong" target="_parent" href="mailto:elin.steen@ka.se?Subject=Saknad%20yrkestitel&body=' + mailBody + '">Skicka mejl</a>' ;
       }
     document.getElementById('resultdiv').appendChild(node);
@@ -532,7 +533,9 @@ function doStuff() {
       }
     }
     if (hittad === true) {
-      messagediv.innerHTML += '<h3 class="u-textMetaDeca paddingTop10" style="font-weight: bold">Finns inte ditt avtal?</h3><p>Finns ditt yrke i listan men ditt avtal kommer inte upp? Mejla oss din yrkestitel och vilket avtal du tillhör så kollar vi på det.</p><a class="homestyled u-textMeta u-textStrong" target="_parent" href="mailto:elin.steen@ka.se?Subject=Saknat%20avtal">Skicka mejl</a>';
+
+      let mailBody = '%0A%0A(Mail från tjänsten OB Storhelg.)'
+      messagediv.innerHTML += '<h3 class="u-textMetaDeca paddingTop10" style="font-weight: bold">Finns inte ditt avtal?</h3><p>Finns ditt yrke i listan men ditt avtal kommer inte upp? Mejla oss din yrkestitel och vilket avtal du tillhör så kollar vi på det.</p><a class="homestyled u-textMeta u-textStrong" target="_parent" href="mailto:elin.steen@ka.se?Subject=Saknat%20avtal&body=' + mailBody + '">Skicka mejl</a>';
     }
 
     // messagediv.innerHTML = string;
