fe5ff6410870a53446cd88e8dccbe5a2d42113ed
elinsteenKA <elin.steen@ka.se>
2025-02-17 16:31:18 +0100

    Add files via upload

--- quiz_drogtest/quiz.js
+++ quiz_drogtest/quiz.js
@@ -180,3 +180,19 @@ $.ajax({
             console.log('Network error has occurred please try again!');
         }
         });
+
+
+const body = document.querySelector('body');
+
+//if todays date is higher than 9 march change background color   
+(function() {
+  var today = new Date();
+  var date = new Date("2025-03-09");
+  if (today > date) {
+    body.style.backgroundColor = 'rgb(249,249,247)';
+  }
+  else {
+    body.style.backgroundColor = '#fcfaf5'
+  }
+})();
+      
