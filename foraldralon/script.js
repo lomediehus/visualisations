d0b4e0111bab9f7952078432da6a163370361ded
elinsteenKA <elin.steen@ka.se>
2025-02-18 08:50:51 +0100

    Add files via upload

--- foraldralon/script.js
+++ foraldralon/script.js
@@ -336,3 +336,18 @@ document.body.addEventListener("touchstart", removeFocus);
 
 //set focus on input field
 document.getElementById('sokYrkeKnapp').focus();
+
+const body = document.querySelector('body');
+
+  //if todays date is higher than 9 march change background color   
+  (function() {
+    var today = new Date();
+    var date = new Date("2025-03-09");
+    if (today > date) {
+      body.style.backgroundColor = 'rgb(249,249,247)';
+    }
+    else {
+      body.style.backgroundColor = '#fcfaf5'
+    }
+  })();
+
