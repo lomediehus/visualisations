3212377faa32fa64a3503ce5e2537b0c9d2ff5e8
elinsteenKA <elin.steen@ka.se>
2025-03-31 10:21:30 +0200

    Add files via upload

---
+++ 25-/pensionsar/pension.js
@@ -0,0 +1,282 @@
+
+// const birthYearArray = [1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987];
+
+const filearray = [
+  {
+    "Födelseår": 1957,
+    "Riktålder": 65,
+    "Lägsta pensionsålder": 61
+  },
+  {
+    "Födelseår": 1958,
+    "Riktålder": 66,
+    "Lägsta pensionsålder": 61
+  },
+  {
+    "Födelseår": 1959,
+    "Riktålder": 66,
+    "Lägsta pensionsålder": 62
+  },
+  {
+    "Födelseår": 1960,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 62
+  },
+  {
+    "Födelseår": 1961,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 63
+  },
+  {
+    "Födelseår": 1962,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 63
+  },
+  {
+    "Födelseår": 1963,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1964,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1965,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1966,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1967,
+    "Riktålder": 67,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1968,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1969,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1970,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 64
+  },
+  {
+    "Födelseår": 1971,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1972,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1973,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1974,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1975,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1976,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1977,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1978,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1979,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1980,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1981,
+    "Riktålder": 68,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1982,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1983,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1984,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 65
+  },
+  {
+    "Födelseår": 1985,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1986,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1987,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1988,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1989,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1990,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1991,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1992,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1993,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1994,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1995,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1996,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1997,
+    "Riktålder": 69,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1998,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 1999,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 2000,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 66
+  },
+  {
+    "Födelseår": 2001,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 67
+  },
+  {
+    "Födelseår": 2002,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 67
+  },
+  {
+    "Födelseår": 2003,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 67
+  },
+  {
+    "Födelseår": 2004,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 67
+  },
+  {
+    "Födelseår": 2005,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 67
+  },
+  {
+    "Födelseår": 2006,
+    "Riktålder": 70,
+    "Lägsta pensionsålder": 67
+  }
+];
+
+var p1 = document.getElementById('result1').firstElementChild.nextElementSibling;
+var p2 = document.getElementById('result2').firstElementChild.nextElementSibling;
+
+const valjar = document.getElementById('valjar');
+
+valjar.addEventListener('change', function() {
+  match(filearray);
+})
+
+var match = function(array) {
+
+  for (var item in array) {
+    if (array[item]['Födelseår'].toString() === valjar.value) {
+      console.log(valjar.value)
+      console.log('Lägsta pensionsålder: ' + array[item]['Lägsta pensionsålder'] + ' år ' + "Riktålder: " + array[item]["Riktålder"] + " år.");
+      if (valjar.value === "Välj födelseår") {
+        p1.innerHTML = "?";
+        p2.innerHTML = "?";
+      }
+      else {
+        p1.innerHTML = array[item]["Lägsta pensionsålder"];
+        p2.innerHTML = array[item]["Riktålder"];
+      }
+    }
+  }
+}
