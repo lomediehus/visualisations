6792e64c5cd426a5e3d88b149386dbe76cd7ceb7
elinsteenKA <elin.steen@ka.se>
2025-10-13 13:12:26 +0200

    Add files via upload

---
+++ framgang/banner.js
@@ -0,0 +1,13 @@
+gsap.registerPlugin();
+
+let tl = gsap.timeline({ repeat: -1, repeatDelay: 0 }); // Repeat infinitely
+
+let xValue = 500;
+
+// Define the animation sequence for each text
+tl.fromTo("#b_text1", { x: xValue + "%", opacity: 0 }, { x: "0%", opacity: 1, duration: 1 })
+  .to("#b_text1", { x: "-"+xValue + "%", opacity: 0, duration: 1, delay: 2 }) // Stay in the middle for 2 secs, then move left
+  .fromTo("#b_text2", { x: xValue + "%", opacity: 0 }, { x: "0%", opacity: 1, duration: 1 })
+  .to("#b_text2", { x: "-"+xValue + "%", opacity: 0, duration: 1, delay: 2 })
+  .fromTo("#b_text3", { x: xValue + "%", opacity: 0 }, { x: "0%", opacity: 1, duration: 1 })
+  .to("#b_text3", { x: "-" + xValue + "%", opacity: 0, duration: 1, delay: 2 });
